23ce50e864d4e05e02e8242aa645cc704f16f369
ramirune <timpiga3@hotmail.com>
2022-11-27 20:13:56 +0100

    searchBar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = () => {
+	return (
+		<View style={styles.backgroundStyle}>
+			<Feather name='search' style={styles.iconStyle} />
+			<TextInput style={styles.inputStyle} placeholder='Search' />
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	backgroundStyle: {
+		backgroundColor: 'rgba(0,0,0,0.05)',
+		marginTop: 10,
+		height: 50,
+		borderRadius: 5,
+		marginHorizontal: 15,
+		flexDirection: 'row',
+	},
+	inputStyle: {
+		flex: 1,
+		fontSize: 18,
+	},
+	iconStyle: {
+		fontSize: 35,
+		alignSelf: 'center',
+		marginHorizontal: 15,
+	},
+});
+
+export default SearchBar;
